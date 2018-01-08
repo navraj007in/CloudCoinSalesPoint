e010dadb6165fe9b25ca00fdd31e0fc0b0672591
navraj.singh <navraj.singh@infodartmail.com>
2018-01-08 14:28:47 +0530

    Initial Commit

---
+++ CloudCoinStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CloudCoinStore.MvcApplication" Language="C#" %>
---
+++ CloudCoinStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CloudCoinStore.Startup))]
+namespace CloudCoinStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
